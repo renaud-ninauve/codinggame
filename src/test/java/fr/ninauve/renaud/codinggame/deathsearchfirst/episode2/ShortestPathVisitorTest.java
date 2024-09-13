d3d259e5560fa4f738423ffaffa81a420b9321ec
renaud-ninauve <renaud.ninauve.92@gmail.com>
2024-09-13 09:32:15 +0200

    several destinations

--- src/test/java/fr/ninauve/renaud/codinggame/deathsearchfirst/episode2/ShortestPathVisitorTest.java
+++ src/test/java/fr/ninauve/renaud/codinggame/deathsearchfirst/episode2/ShortestPathVisitorTest.java
@@ -1,12 +1,9 @@
 package fr.ninauve.renaud.codinggame.deathsearchfirst.episode2;
 
-import net.bytebuddy.asm.Advice;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Stream;
@@ -16,15 +13,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class ShortestPathVisitorTest {
     private static final int START = 1_000_000;
-    private static final int DESTINATION = 9_999_999;
+    private static final int DESTINATION1 = 1_999_999;
+    private static final int DESTINATION2 = 2_999_999;
 
     static class ShortestPathVisitor implements Node.Visitor {
-        private final Node destination;
+        private final List<Node> destinations;
         private Path shortest;
         private Path currentPath;
 
-        ShortestPathVisitor(Node destination) {
-            this.destination = destination;
+        ShortestPathVisitor(List<Node> destinations) {
+            this.destinations = destinations;
         }
 
         @Override
@@ -36,7 +34,7 @@ public class ShortestPathVisitorTest {
                 return false;
             }
             currentPath = currentPath.withNewNode(node);
-            if (node == destination) {
+            if (destinations.contains(node)) {
                 if (shortest == null || currentPath.compareTo(shortest) < 0) {
                     shortest = currentPath;
                 }
@@ -58,9 +56,10 @@ public class ShortestPathVisitorTest {
         return Stream.of(Arguments.of(
                         network()
                                 .link(START, 2)
-                                .link(2, DESTINATION)
+                                .link(2, DESTINATION1)
                                 .build(),
-                        List.of(START, 2, DESTINATION)),
+                        List.of(DESTINATION1),
+                        List.of(START, 2, DESTINATION1)),
 
                 Arguments.of(
                         network()
@@ -68,20 +67,35 @@ public class ShortestPathVisitorTest {
                                 .link(2, 3)
                                 .link(3, 4)
                                 .link(4, 5)
-                                .link(5, DESTINATION)
+                                .link(5, DESTINATION1)
                                 .link(2, 6)
-                                .link(6, DESTINATION)
+                                .link(6, DESTINATION1)
                                 .build(),
-                        List.of(START, 2, 6, DESTINATION)
-                ));
+                        List.of(DESTINATION1),
+                        List.of(START, 2, 6, DESTINATION1)
+                ),
+
+                Arguments.of(
+                        network()
+                                .link(START, 2)
+                                .link(2, 3)
+                                .link(3, DESTINATION1)
+                                .link(START, 4)
+                                .link(4, DESTINATION2)
+                                .build(),
+                        List.of(DESTINATION1, DESTINATION2),
+                        List.of(START, 4, DESTINATION2))
+        );
     }
 
     @ParameterizedTest
     @MethodSource
-    void find_shortest(Network network, List<Integer> expected) {
+    void find_shortest(Network network, List<Integer> destinations, List<Integer> expected) {
         Node start = network.getNode(START);
-        Node destination = network.getNode(DESTINATION);
-        ShortestPathVisitor visitor = new ShortestPathVisitor(destination);
+        List<Node> destinationNodes = destinations.stream()
+                .map(network::getNode)
+                .toList();
+        ShortestPathVisitor visitor = new ShortestPathVisitor(destinationNodes);
         start.visitDepthFirst(visitor, Comparator.comparing(Node::value));
         List<Integer> actual = visitor.shortest.stream()
                 .map(Node::value)
